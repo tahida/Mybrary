feefbd0528f35a0d2a1b9bc472afba564d3f1952
tahida <htaufiq024@gmail.com>
2022-01-04 03:29:53 +0700

    Initial Author Routes

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(express.static('public'))
 app.use(bodyParser.urlencoded({ limit: '10mb', extended: false }))
 
 const mongoose = require('mongoose')
-mongoose.connect(process.env.MONGODB_URI, {useNewUrlParser: true})
+mongoose.connect(process.env.DATABASE_URL, {useNewUrlParser: true})
 const db = mongoose.connection
 db.on('error', error => console.error(error))
 db.once('open', () => console.log('Connected to Mongoose'))
